fff65a8724e5e55b1ec80374f014c84a56331df9
Tomasz Steć <tomeczekstecc@gmail.com>
2020-10-23 11:39:16 +0200

    actions

---
+++ .cache/plugins/strapi-plugin-upload/admin/src/translations/index.js
@@ -0,0 +1,15 @@
+import en from './en.json';
+import ja from './ja.json';
+import ms from './ms.json';
+import ru from './ru.json';
+import uk from './uk.json';
+
+const trads = {
+  en,
+  ja,
+  ms,
+  ru,
+  uk,
+};
+
+export default trads;
